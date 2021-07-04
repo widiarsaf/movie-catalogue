da1674fb5430ca4610611f14cffc756557ea8024
widiarsaf <wretasafitri33@gmail.com>
2021-07-04 20:30:29 +0700

    show data from API request

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  KEY: 'e6637935eaa9658bbf25fdb408cc7981',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+};
+
+export default CONFIG;
