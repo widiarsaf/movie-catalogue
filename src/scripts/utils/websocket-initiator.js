9a753342efccd3e4beb73eb4682adb0092a9e5d1
widiarsaf <wretasafitri33@gmail.com>
2021-07-06 14:31:02 +0700

    send notification through websocket

--- src/scripts/utils/websocket-initiator.js
+++ src/scripts/utils/websocket-initiator.js
@@ -1,3 +1,6 @@
+import NotificationHelper from './notification-helper';
+import CONFIG from '../globals/config';
+
 const WebSocketInitiator = {
   init(url) {
     const webSocket = new WebSocket(url);
@@ -5,7 +8,16 @@ const WebSocketInitiator = {
   },
 
   _onMessageHandler(message) {
-    console.log(message.data);
+    // console.log('ok');
+    const movie = JSON.parse(message.data);
+    NotificationHelper.sendNotification({
+      title: `${movie.title} is on cinema!`,
+      options: {
+        body: movie.overview,
+        image: `${CONFIG.BASE_IMAGE_URL + movie.poster_path}`,
+      },
+    });
   },
 };
+
 export default WebSocketInitiator;
